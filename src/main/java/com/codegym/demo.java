91a3a3c2b4b6be12265f0e61058909aae8da01c2
ngoclan1102 <nguyenthimgoclan@gmail.com>
2022-03-10 09:24:02 +0700

    uppppp

---
+++ src/main/java/com/codegym/demo.java
@@ -0,0 +1,30 @@
+package com.codegym;
+
+public class demo {
+    private int id;
+    private String name;
+
+    public demo(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public demo() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
